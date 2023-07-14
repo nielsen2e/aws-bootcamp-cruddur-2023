d182c633f8def8de15ea4ed2e6945ab381966064
Daniel Amadi <danielamadi000@gmail.com>
2023-07-14 13:07:35 +0000

    add seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Daniel Amadi', 'danielamadi' ,'danielamadi000@gmail.com' , 'MOCK'),
   ('Kachi Amadi', 'kachiamadi' ,'princedanny922@gmail.com' , 'MOCK'),
-   ('James Amadi', 'jamesamadi' ,'james@gmail.com' , 'MOCK');
+  ('James Amadi', 'jamesamadi' ,'james@gmail.com' , 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
